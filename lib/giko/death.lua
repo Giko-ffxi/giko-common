8f0c336ce96579c66883668a81136abff5ac6d44
Giko <giko.ffxi@gmail.com>
2022-10-08 14:55:30 -0400

    Bugfix: get_tod

--- lib/giko/death.lua
+++ lib/giko/death.lua
@@ -8,14 +8,14 @@ local death   = { memory = {}, cache = string.format('%s\\..\\giko-cache\\cache\
 death.get_tod = function(name)
 
     local mob = monster.get(name)
-    local tod = mob ~= nil and common.in_array_key(death.memory, name) and death.memory[name] or cache.get(death.cache, mob.names.nq[1])
+    local tod = mob ~= nil and common.in_array_key(death.memory, mob.names.nq[1]) and death.memory[mob.names.nq[1]] or (mob ~= nil and cache.get(death.cache, mob.names.nq[1]))
 
-    if mob ~= nil then
+    if mob ~= nil and tod ~= nil then
 
         death.memory[mob.names.nq[1]] = tod ~= nil and tod or json:encode(nil)
 
         if tod ~= nil then
-            return tod ~= nil and json:decode(tod) or nil
+            return json:decode(tod)
         end
 
     end
@@ -43,7 +43,7 @@ death.set_tod = function(name, gmt, day)
     if mob ~= nil then
 
         death.memory[mob.names.nq[1]] = json:encode({name = mob.names.nq[1], gmt = gmt, day = day, created_at = os.time()})
-        cache.set(death.cache, mob.names.nq[1], death.memory[mob.names.nq[1]])
+        return cache.set(death.cache, mob.names.nq[1], death.memory[mob.names.nq[1]])
 
     end
 
@@ -57,7 +57,7 @@ death.set_day = function(name, day)
     if mob ~= nil and tod ~= nil then
 
         death.memory[mob.names.nq[1]] = json:encode({name = tod.name, gmt = tod.gmt, day = day, created_at = os.time()})
-        cache.set(death.cache, mob.names.nq[1], death.memory[mob.names.nq[1]])
+        return cache.set(death.cache, mob.names.nq[1], death.memory[mob.names.nq[1]])
         
     end
 
